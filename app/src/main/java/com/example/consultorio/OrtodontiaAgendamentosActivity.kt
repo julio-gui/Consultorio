0d974c1683dff41afd99613d6be901791cb6a94a
julio-gui <juliognb2@hotmail.com>
2025-06-11 10:38:01 -0300

    - Correção na activity da tela de agendamentos de ortodontia  para ficar de acordo com as alterações nos layouts.

--- app/src/main/java/com/example/consultorio/OrtodontiaAgendamentosActivity.kt
+++ app/src/main/java/com/example/consultorio/OrtodontiaAgendamentosActivity.kt
@@ -1,6 +1,7 @@
 package com.example.consultorio
 
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
@@ -20,18 +21,23 @@ class OrtodontiaAgendamentosActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.agendamentos)
-
+        val imgBack1: ImageView = findViewById(R.id.imgBack1)
+        val imgBack2: ImageView = findViewById(R.id.imgBack2)
         val titulo = findViewById<TextView>(R.id.titulo)
-        titulo.text = "Agendamentos - Ortodontia"
-
+        titulo.text = "Ortodontia"
         recyclerView = findViewById(R.id.recycler_agendamentos)
         recyclerView.layoutManager = LinearLayoutManager(this)
-
         adapter = AgendamentoAdapter(listaAgendamentos) { agendamento, position ->
             mostrarDialogoFinalizar(agendamento, position)
         }
         recyclerView.adapter = adapter
+        imgBack1.setOnClickListener {
+            onBackPressedDispatcher.onBackPressed()
+        }
 
+        imgBack2.setOnClickListener {
+            onBackPressedDispatcher.onBackPressed()
+        }
         carregarAgendamentosOrtodontia()
     }
 
@@ -48,7 +54,8 @@ class OrtodontiaAgendamentosActivity : AppCompatActivity() {
                     .limit(1)
                     .get()
                     .addOnSuccessListener { docs ->
-                        if (!docs.isEmpty) {
+                        if (docs.isEmpty) {
+                        } else {
                             val doc = docs.documents[0].reference
                             doc.update("finalizado", true)
                                 .addOnSuccessListener {
@@ -62,7 +69,6 @@ class OrtodontiaAgendamentosActivity : AppCompatActivity() {
             .setNegativeButton("Voltar", null)
             .show()
     }
-
 
     private fun carregarAgendamentosOrtodontia() {
         firestore.collection("agendamentos")
